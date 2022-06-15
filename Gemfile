021938047fe8e18c6cba0ac9d66878c1e158dad5
Pierre S <pierre.sarazin18@gmail.com>
2022-06-15 17:23:18 +0200

    first commit

--- Gemfile
+++ Gemfile
@@ -51,3 +51,5 @@ end
 gem "devise", "~> 4.8"
 gem "devise-jwt", "~> 0.9.0"
 gem "rack-cors", "~> 1.1"
+
+gem "dotenv-rails", "~> 2.7"
--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -13,7 +13,7 @@ class MembersController < ApplicationController
 
   def get_user_from_token
     jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1],
-                             Rails.application.credentials.devise[:jwt_secret_key]).first
+                              ENV['DEVISE_JWT_SECRET_KEY']).first
     user_id = jwt_payload['sub']
     User.find(user_id.to_s)
   end
